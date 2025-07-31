fb0e14f3f27b875a46f154fefe6619f46a2092a6
Ula <73685848+novia-aa@users.noreply.github.com>
2025-07-31 08:04:10 -0500

    Create 238_Products of Array Except Self.py

---
+++ array-hashing/238_Products of Array Except Self.py
@@ -0,0 +1,18 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        answer = [1] * n
+
+        # Prefix product
+        prefix = 1
+        for i in range(n):
+            answer[i] = prefix
+            prefix *= nums[i]
+
+        # Postfix product
+        postfix = 1
+        for i in range(n - 1, -1, -1):
+            answer[i] *= postfix
+            postfix *= nums[i]
+
+        return answer
